6c9bd7370c80729111e06b291e92cabb2c5faab8
Baris Ucakturk <baris.ucakturk@gmail.com>
2019-03-02 14:31:56 +0300

    Initial commit

---
+++ src/main/java/com/ucakturk/searchflight/SearchFlightApplication.java
@@ -0,0 +1,13 @@
+package com.ucakturk.searchflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchFlightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucakturk/searchflight/SearchFlightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ucakturk.searchflight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchFlightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
